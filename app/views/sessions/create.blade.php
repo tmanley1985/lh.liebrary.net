069b860101d27a30765bb9226d349ecd1d086b80
tmanley1985 <tmanley1985@gmail.com>
2015-02-09 21:33:39 -0500

    Created directory structure and views for login and logout

---
+++ app/views/sessions/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.main')
+@section('content')
+	
+	<div id="content">
+	  <div class="container">
+	    <div class="row">
+		  <div class="span12">
+		    <div class="well well-form txt-lefty">
+              <h3>Sign In</h3>
+			  {{ Form::open() }}
+
+		        <div class="control-group">
+		          <div class="controls">
+			        <div class="input-prepend">
+			          <span class="add-on"><i class="icon-user"></i></span>
+					  {{ Form::text('username', null, array(
+			          		'name' => 'username',
+			          		'placeholder' => 'Username',
+			          		'class' => "span3",
+			          		'required' => 'required')
+			          	)}}
+			        </div>
+				  </div>
+			    </div>
+			    <div class="control-group"> 
+		          <div class="controls">
+			        <div class="input-prepend">
+			          <span class="add-on"><i class="icon-lock"></i></span>
+					  {{ Form::password('password',array(
+			          		'name' => 'password',
+			          		'placeholder' => 'Password',
+			          		'class' => "span3",
+			          		'required' => 'required')
+			          	)}}
+			        </div>
+				  </div>			
+		        </div>
+                <div class="form-actions">
+			     <!-- <label class="checkbox">
+                    <input type="checkbox" name="remember">
+                    Remember Me
+                  </label> -->
+			      <button class="btn btn-primary" type="submit">Sign In</button>
+                </div> 
+              {{ Form::close() }}
+		     <!-- <div class="form-extra">
+			    <button class="btn btn-info"><i class="glyphicons-twitter"></i> Login with Twitter</button>
+			    <button class="btn btn-danger"><i class="glyphicons-google_plus"></i> Login with Google</button>
+			  </div> -->			  
+            </div>
+		  </div>  
+		</div>  	
+	  </div>
+	</div>	
+@stop
