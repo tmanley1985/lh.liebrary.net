fdf20ae264089d59abd75f111ed426b843e9e27e
tmanley1985 <tmanley1985@gmail.com>
2015-02-09 20:21:19 -0500

    Added isValid function and set rules for validation

--- app/models/User.php
+++ app/models/User.php
@@ -8,12 +8,23 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
-
+	public $timestamps = false;
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
+
+	public static $rules = [
+		'firstname' => 'required',
+		'lastname' => 'required',
+		'email' => 'required|email|unique:users',
+		'username' => 'required|unique:users|min:6',
+		'password' => 'required'
+	];
+
+	public static $messages;
+
 	protected $table = 'users';
 
 	/**
@@ -21,6 +32,21 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
+	//protected $hidden = array('password', 'remember_token');
+
+	//Checks whether input is valid against static rules method
+
+	public static function isValid($data)
+	{
+		$validation = Validator::make($data, static::$rules);
+
+		if($validation->passes()){
+			return true;
+		}
+
+		static::$messages = $validation->messages();
+
+		return false;
+	}
 
 }
