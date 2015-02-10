a5615af27aee59cf932ec605beaae41108c6181e
tmanley1985 <tmanley1985@gmail.com>
2015-02-09 20:25:50 -0500

    Added directory structure for static pages

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.main')
+@section('content')
+	
+	<div id="landing">
+	  <div class="container">  
+	    <div class="row">
+		  <div class="span5">
+		    <h1>Ever been Lied to?</h1>	
+			<p>It's our guess that you have at one time or another. Wish you
+				could keep track of who has lied to you and what those lies were?
+				Want to save it for ammunition in a heated discussion for later?
+				We're not judging.  Now you can!</p>
+		    <p><a href="#" class="btn btn-primary btn-action">Get Started Now</a></p>  
+		  </div>
+		  <div class="span7">
+		    <div class="js-video">
+			  <iframe src="http://player.vimeo.com/video/38498748?title=0&amp;byline=0&amp;portrait=0&amp;color=38e8ae" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+		    </div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
+	<div id="content" class="txt-middle">
+	  <div class="container">
+	    <div class="row">
+		  <div class="span12">
+			<h2>Create your own Liebrary today!</h2>
+		    <hr class="dashed">				
+		  </div>  
+		</div>  
+	    <div class="row">
+		  <div class="span4 bouncy">
+		    <div class="icon_wrapper">
+		      <i class="glyphicons-display animated"></i>
+		    </div>
+			<h2>Modern and Minimalistic</h2>
+			<p>Liebrary has a modern Content Management System for all of our users.</p>	
+		  </div>
+		  <div class="span4 bouncy">
+		    <div class="icon_wrapper">
+		      <i class="glyphicons-font animated"></i>
+		    </div>
+			<h2>24/7 Support</h2>
+			<p>We do not sleep. 'Nuff Said.</p>	
+		  </div>
+		  <div class="span4 bouncy">
+		    <div class="icon_wrapper">
+		      <i class="glyphicons-airplane animated"></i>
+		    </div>
+			<h2>Completely Free</h2>
+			<p>We said it's free.  Not a lot of verbal clutter there.</p>	
+		  </div>		  
+		</div>
+		<hr class="dashed">				
+	    <div class="row">
+		  <div class="span4">
+			<a class="thumbnail" href="">
+			  <img alt="image" src="http://quickimage.it/400x200">
+			</a>		
+			<h2>Image 1</h2>
+			<p>Optional subtext to help better explain the image above can go here.</p>
+		  </div>
+		  <div class="span4">
+			<a class="thumbnail" href="">
+			  <img alt="image" src="http://quickimage.it/400x200">
+			</a>
+			<h2>Image 2</h2>
+			<p>Optional subtext to help better explain the image above can go here.</p>
+		  </div>
+		  <div class="span4">
+			<a class="thumbnail" href="">
+			  <img alt="image" src="http://quickimage.it/400x200">
+			</a>
+			<h2>Image 3</h2>
+			<p>Optional subtext to help better explain the image above can go here.</p>
+		  </div>		  
+		</div>		
+	  </div>
+	</div>
+@stop
+
