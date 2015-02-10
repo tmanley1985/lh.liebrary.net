c494603c2adc1f7ed9958bbad4860ab11f30cc56
tmanley1985 <tmanley1985@gmail.com>
2015-02-09 20:24:19 -0500

    Added folder structure for included files as well as header, nav and footer files

---
+++ app/views/includes/footer.blade.php
@@ -0,0 +1,59 @@
+<div id="footer">
+	  <div class="container">
+	    <div class="row">
+		  <div class="span3">
+			<h3>Quick Links</h3>
+			<ul class="animated">
+			  <li><a href="#">Home</a></li>
+			  <li><a href="#">About Us</a></li>
+			  <li><a href="#">Features</a></li>
+			  <li><a href="#">Pricing</a></li>
+			</ul>			
+		  </div>
+		  <div class="span3">		
+			<h3>Company</h3>
+			<ul class="animated">
+			  <li><a href="#">Privacy Policy</a></li>
+			  <li><a href="#">Terms of Use</a></li>
+			  <li><a href="#">FAQ</a></li>
+			</ul>	
+		  </div>
+		  <div class="span3">
+			<h3>We're Social</h3>
+			<ul class="animated">
+			  <li><a href="#">Facebook</a></li>
+			  <li><a href="#">Twitter</a></li>
+			  <li><a href="#">Google &#43;</a></li>
+			</ul>		  
+		  </div>
+		  <div class="span3">
+			<h3>Subscribe</h3>
+			<p>Subscribe to our newsletter and stay up to date with the latest news and deals!</p>			
+		    <form>
+              <input class="span3" type="text" placeholder="Your Email" name="subscribe">
+			  <button class="btn btn-primary">Subscribe</button>
+			</form>
+		  </div>		  
+		</div>	  
+      </div>	  
+    </div>
+
+	<div id="copywrite">
+	  <div class="container">
+	    <div class="row">
+		  <div class="span12">
+		    <p>&copy; 2015 Liebrary.net<span id="totop" class="pull-right">Back to Top <i class="icon-arrow-up icon-white"></i></span></p>			
+		  </div>		  
+		</div>	  
+      </div>	  
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.8.3.min.js"><\/script>')</script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.fitvids.js"></script>
+	<script src="assets/js/jqBootstrapValidation.js"></script>
+	<script src="assets/js/bounce.js"></script>	
+
+  </body>
+</html>
---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Liebrary| Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Thomas A. Manley">
+	
+    <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{ asset('assets/css/bootstrap-responsive.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/css/glyphicons.css') }}" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href="{{ asset('assets/css/base.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/css/blue.css') }}" rel="stylesheet">
+	
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+	
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('assets/co/apple-touch-icon-114-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/ico/favicon.png') }}">
+  </head>  
+
+  <body>
