23895443432ea4c5755b82fab846230f0d063e3d
tmanley1985 <tmanley1985@gmail.com>
2015-02-23 21:27:40 -0500

    Cleaned up code structure

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -3,6 +3,29 @@
 class SessionsController extends BaseController
 {
 	public function create(){
+		if(Auth::check()){
+
+			return Redirect::to('/');
+		}
 		return View::make('sessions.create');
+	}
+
+	public function store()
+	{
+		// Checks attempt to login against input
+		if (Auth::attempt(Input::only('username', 'password'))){
+
+			return Redirect::to('/');
+	}
+
+		return "Fails";
+	}
+
+	public function destroy(){
+
+		//Logs the user out
+		Auth::logout();
+
+		return Redirect::to('/login');
 	}
 }
